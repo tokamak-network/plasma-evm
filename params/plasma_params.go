0b3c468d2bc02378e1c5b62869e21766fc6a1749
4000D <dkdkajej@gmail.com>
2018-10-16 13:37:47 +0900

    params: plasma parameters fixed & Operator added

--- params/plasma_params.go
+++ params/plasma_params.go
@@ -7,5 +7,8 @@ import (
 
 var (
 	NullAddress = common.Address{0x0000000000000000000000000000000000000000}
-	NullKey, _ = crypto.HexToECDSA("0x00")
+	NullKey, _  = crypto.HexToECDSA("00")
+
+	// address of b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291
+	Operator = common.HexToAddress("0x71562b71999873DB5b286dF957af199Ec94617F7")
 )
