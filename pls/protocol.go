314b954ff0eeba93b07ec0744c73f0c5e654a36d
thomashin <dn2757@gmail.com>
2018-10-16 13:12:37 +0900

    pls: change protocol name

--- pls/protocol.go
+++ pls/protocol.go
@@ -35,9 +35,9 @@ const (
 )
 
 // ProtocolName is the official short name of the protocol used during capability negotiation.
-var ProtocolName = "eth"
+var ProtocolName = "pls"
 
-// ProtocolVersions are the upported versions of the eth protocol (first is primary).
+// ProtocolVersions are the upported versions of the pls protocol (first is primary).
 var ProtocolVersions = []uint{eth63, eth62}
 
 // ProtocolLengths are the number of implemented message corresponding to different protocol versions.
@@ -45,7 +45,7 @@ var ProtocolLengths = []uint64{17, 8}
 
 const ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
 
-// eth protocol message codes
+// pls protocol message codes
 const (
 	// Protocol messages belonging to eth/62
 	StatusMsg          = 0x00
