dea30c8b791c268693ce1bc3ce5b713123653682
4000D <dkdkajej@gmail.com>
2018-10-23 13:41:34 +0900

    contracts/plasma: requestable simple token added

--- contracts/plasma/plasma.go
+++ contracts/plasma/plasma.go
@@ -1,3 +1,4 @@
 package plasma
 
 // go:generate abigen --sol plasma-evm-cotracts/contracts/RootChain.sol --pkg contract --out contract/rootchain.go
+// go:generate abigen --sol plasma-evm-cotracts/contracts/RequestableSimpleToken.sol --pkg contract --out contract/requestable_simple_token.go
