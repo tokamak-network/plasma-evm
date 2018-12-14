33c3ad0456948b96fdf51c7686f45cee88cdb442
Dapploper <gonlgonl@naver.com>
2018-12-14 15:30:05 +0900

    params: add SubmitBlockGasPrice and Limit

--- params/plasma_params.go
+++ params/plasma_params.go
@@ -14,6 +14,9 @@ var (
 	// address of b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291
 	Operator = common.HexToAddress("0x71562b71999873DB5b286dF957af199Ec94617F7")
 
+	SubmitBlockGasPrice        = big.NewInt(1e9)
+	SubmitBlockGasLimit uint64 = 4000000
+
 	RequestTxGasPrice        = big.NewInt(1e9)
 	RequestTxGasLimit uint64 = 100000
 )
