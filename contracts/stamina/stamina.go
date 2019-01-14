c2dff98168ae785c8a9479c1972bb9e74865893a
Geonwoo, Shin <dn2757@gmail.com>
2019-01-14 19:45:07 +0900

    contracts/stamina: make core/stamina test code

--- contracts/stamina/stamina.go
+++ contracts/stamina/stamina.go
@@ -1,19 +1,29 @@
-// Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package stamina
 
 //go:generate abigen --sol contract/Stamina.sol --pkg contract --out contract/stamina.go
+
+import (
+	"github.com/Onther-Tech/plasma-evm/accounts/abi/bind"
+	"github.com/Onther-Tech/plasma-evm/common"
+	"github.com/Onther-Tech/plasma-evm/contracts/stamina/contract"
+)
+
+type Stamina struct {
+	*contract.StaminaSession
+	contractBackend bind.ContractBackend
+}
+
+func NewStamina(transactOpts *bind.TransactOpts, contractAddr common.Address, contractBackend bind.ContractBackend) (*Stamina, error) {
+	stamina, err := contract.NewStamina(contractAddr, contractBackend)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Stamina{
+		&contract.StaminaSession{
+			Contract:     stamina,
+			TransactOpts: *transactOpts,
+		},
+		contractBackend,
+	}, nil
+}
