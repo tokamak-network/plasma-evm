4fdb60b8378c16f618b23c15a462e40b02b957f5
Jin <jin.s@onther.io>
2019-03-06 16:39:10 +0900

    dockerize, create all new files for docker container

--- docker.user.pls.sh
+++ docker.user.pls.sh
@@ -3,6 +3,24 @@ DATADIR=$HOME/.pls.dev
 
 rm -rf $DATADIR
 
+function get_rootchain() {
+  ROOTCHAIN=`curl -X POST --data '{"jsonrpc":"2.0","method":"eth_rootChain","params":[],"id":67}' -H "Content-Type: application/json" $OPERATOR_IP:8547 --silent |  sed -n 's/.*\(0x.*\)\".*/\1/p'`
+}
+
+# First Try to get RootChain address from childchain.
+get_rootchain
+
+# condition check
+# address_count=$(echo -n $ROOTCHAIN | wc -m)
+
+while [ $(echo ${#ROOTCHAIN}) != 42 ];
+do
+  echo "Waiting for deploy rootchain contract"
+  sleep 1s
+  get_rootchain
+done
+echo "Got RootChain address from Child Chain" $ROOTCHAIN
+
 geth \
   --datadir $DATADIR \
   --rpc \
@@ -10,7 +28,7 @@ geth \
   --rpcaddr 0.0.0.0 \
   --rpcport 8549 \
   --port 30307 \
-  --rootchain.url "ws://172.30.0.3:8546" \
+  --rootchain.url "ws://${ROOTCHAIN_IP}:8546" \
   --rootchain.contract "0x880EC53Af800b5Cd051531672EF4fc4De233bD5d" \
   --syncmode "full" \
-  --bootnodes "enode://401bd6383fe11a5224d5b4277b53d7c0278efed3ca685b6593935751ad1fe734a8e35d2b3ebd9d7fc6da6cff12e72cfcfca8db408bf2e49f1fad4c503956d07f@172.30.0.6:30307"
+  --bootnodes enode://401bd6383fe11a5224d5b4277b53d7c0278efed3ca685b6593935751ad1fe734a8e35d2b3ebd9d7fc6da6cff12e72cfcfca8db408bf2e49f1fad4c503956d07f@172.30.0.6:30301
