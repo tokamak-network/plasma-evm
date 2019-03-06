3a0fbabcc062627345b2f192d43736d14bcb82c7
Jin <jin.s@onther.io>
2019-03-06 16:39:10 +0900

    docker, Renew base testchain settting

--- docker.run.pls.sh
+++ docker.run.pls.sh
@@ -21,8 +21,10 @@ geth \
   --rpc \
   --rpcaddr 0.0.0.0 \
   --rpcport 8547 \
+  --port 30301 \
   --dev.key $OPERATOR_KEY,$KEY1,$KEY2,$KEY3,$KEY4,$KEY5,$CHALLENGER_KEY \
   --operator $OPERATOR \
   --rootchain.challenger $CHALLENGER \
   --rootchain.interval "300ms" \
-  --rootchain.url "ws://172.30.0.3:8546"
+  --rootchain.url "ws://172.30.0.3:8546" \
+  --bootnodes enode://401bd6383fe11a5224d5b4277b53d7c0278efed3ca685b6593935751ad1fe734a8e35d2b3ebd9d7fc6da6cff12e72cfcfca8db408bf2e49f1fad4c503956d07f@172.30.0.6:30301
