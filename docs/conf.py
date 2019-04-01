64a2d92d87b0042604cd82eb23392eca690520c6
4000D <dkdkajej@gmail.com>
2019-04-02 05:46:12 +0900

    docs: solidity lexer added

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,17 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
+def setup(sphinx):
+    thisdir = os.path.dirname(os.path.realpath(__file__))
+    sys.path.insert(0, thisdir + '/utils')
+    from pygments_lexer_solidity import SolidityLexer
+    sphinx.add_lexer('Solidity', SolidityLexer())
+
+
 
 
 # -- Project information -----------------------------------------------------
