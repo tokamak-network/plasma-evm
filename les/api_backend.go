luxfi/sdk

les: handler separation (#19639)
les: handler separation

les/api_backend.go
@@ -54,7 +54,7 @@ func (b *LesApiBackend) CurrentBlock() *types.Block {
 }
 
 func (b *LesApiBackend) SetHead(number uint64) {
-	b.eth.protocolManager.downloader.Cancel()
+	b.eth.handler.downloader.Cancel()
 	b.eth.blockchain.SetHead(number)
 }
 

les/bloombits.go
@@ -46,9 +46,10 @@ const (
 func (eth *LightEthereum) startBloomHandlers(sectionSize uint64) {
 	for i := 0; i < bloomServiceThreads; i++ {
 		go func() {
+			defer eth.wg.Done()
 			for {
 				select {
-				case <-eth.shutdownChan:
+				case <-eth.closeCh:
 					return
 
 				case request := <-eth.bloomRequests:

tokamak-network/plasma-evm

les/api_backend.go
@@ -54,7 +54,7 @@ func (b *LesApiBackend) CurrentBlock() *types.Block {
 }
 
 func (b *LesApiBackend) SetHead(number uint64) {
-	b.pls.protocolManager.downloader.Cancel()
+	b.pls.handler.downloader.Cancel()
 	b.pls.blockchain.SetHead(number)
 }
 

les/bloombits.go
@@ -46,9 +46,10 @@ const (
 func (pls *LightEthereum) startBloomHandlers(sectionSize uint64) {
 	for i := 0; i < bloomServiceThreads; i++ {
 		go func() {
+			defer eth.wg.Done()
 			for {
 				select {
-				case <-pls.shutdownChan:
+				case <-pls.closeCh:
 					return
 
 				case request := <-pls.bloomRequests: